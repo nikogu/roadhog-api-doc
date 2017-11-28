fefea4ea2d9bf551515a644d6a8627b86e60c68d
nikogu <644506165@qq.com>
2017-11-28 09:58:08 +0800

    Fixed ignore ../ -> ./ https://github.com/nikogu/roadhog-api-doc/pull/12

--- boilerplate/win-build.js
+++ boilerplate/win-build.js
@@ -2,4 +2,4 @@ var exec = require('child_process').exec;
 var path = require('path');
 var cwd = process.cwd();
 
-exec(path.join(cwd, '../node_modules/.bin/roadhog') + ' build');
+exec(path.join(cwd, './node_modules/.bin/roadhog') + ' build');
